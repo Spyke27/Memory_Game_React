706beeda6122abb46428d0400b3226537423b6c3
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-02-14 22:51:13 -0300

    styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import './App.css';
+import * as C from './App.styles';
+import LogoImage from './assets/devmemory_logo.png';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      
-    </div>
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href="">
+          <img src={LogoImage} width="200" alt="" />
+        </C.LogoLink>
+
+        <C.InfoArea>
+
+        </C.InfoArea>
+
+        <button>reiniciar</button>
+      </C.Info>
+      <C.GridArea>
+
+      </C.GridArea>
+    </C.Container>
   );
 }
 
