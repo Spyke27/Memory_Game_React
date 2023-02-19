4307368f47334ac236d48a7ff8cbfc7c90955c25
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-02-18 21:03:14 -0300

    InfoItem component

--- src/components/InfoItem/index.tsx
+++ src/components/InfoItem/index.tsx
@@ -1,10 +1,16 @@
 import * as C from './styles'
 
-export const InfoItem = () => {
+type Props = {
+    label:string;
+    value:string;
+}
+
+export const InfoItem = ({label, value}: Props) => {
 
     return(
         <C.Container>
-            
+            <C.Label>{label}</C.Label>
+            <C.Value>{value}</C.Value>
         </C.Container>
     )
 }
