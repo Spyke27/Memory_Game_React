559811d48904034e4091017d448d24c6a7784770
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-03-05 23:19:37 -0300

    Button Reset test

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,9 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div``;
 
-export const IconArea = styled.div`
-
-`;
+export const IconArea = styled.div``;
 
 export const Icon = styled.img``;
 
